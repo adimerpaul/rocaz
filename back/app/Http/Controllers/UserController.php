ea7011b40b551ce2c03ee88ca026c230ffeaa3ba
adimerpaul <adimer101@gmail.com>
2024-03-13 20:10:53 -0400

    creado api de login

--- back/app/Http/Controllers/UserController.php
+++ back/app/Http/Controllers/UserController.php
@@ -2,11 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller{
-    public function login(){
-
-
+    public function login(Request $request){
+        $user = User::where('email', $request->email)->first();
+        if($user){
+            if(Hash::check($request->password, $user->password)){
+                $token = $user->createToken('web')->plainTextToken;
+                return response()->json([
+                    'token' => $token,
+                    'user' => $user
+                ]);
+            }
+        }else{
+            return response()->json([
+                'message' => 'Credenciales incorrectas'
+            ], 401);
+        }
+    }
+    public function me(Request $request){
+        return $request->user();
+    }
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Sesión cerrada'
+        ]);
     }
 }
--- back/routes/api.php
+++ back/routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::post('/login', [\App\Http\Controllers\UserController::class, 'login']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/me', [\App\Http\Controllers\UserController::class, 'me']);
+    Route::post('/logout', [\App\Http\Controllers\UserController::class, 'logout']);
 });
